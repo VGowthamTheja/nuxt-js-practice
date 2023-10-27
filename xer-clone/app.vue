3d439c709a118397bf009250113619cc574ff1b7
Vempalli Gowtham Theja <vempalligowthamtheja@gmail.com>
2023-10-28 02:06:04 +0530

    twitter clone first draft

---
+++ xer-clone/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div :class="{'dark': darkMode}">
+    <h1 class="text-red-500 dark:text-dim-500">Twitter Clone</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const darkMode = ref(true)
+</script>
